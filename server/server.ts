7399fd8c512d022ce895971ff6ce0e4a6b9d0bba
aidanprior <priorlyaidan@yahoo.com>
2024-11-20 15:55:57 -0500

    chore: formatting

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
-
 import 'dotenv/config';
 
 const app = express();
@@ -13,7 +12,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 
 app.use(express.static(path.resolve(import.meta.dirname, '../client/assets')));
 
@@ -30,25 +28,22 @@ app.use((req: Request, res: Response) => {
   });
 });
 
-
 /**
  * Global Error Handler
  * @middleware
  */
-app.use(
-  (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-    const DEFAULT_ERROR = {
-      log: 'An Unknown Middleware Error Occurred',
-      status: 500,
-      message: {
-        err: 'A Server Error Occured',
-      },
-    };
-    const specificError = { ...DEFAULT_ERROR, ...err };
-    console.error(specificError.log);
-    res.status(specificError.status).json(specificError.message);
-  },
-);
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  const DEFAULT_ERROR = {
+    log: 'An Unknown Middleware Error Occurred',
+    status: 500,
+    message: {
+      err: 'A Server Error Occured',
+    },
+  };
+  const specificError = { ...DEFAULT_ERROR, ...err };
+  console.error(specificError.log);
+  res.status(specificError.status).json(specificError.message);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port:${port}`);
